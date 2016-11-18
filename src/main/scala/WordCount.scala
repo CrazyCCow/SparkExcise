726cfacfc29033f304908a643945fc7958c875f6
CrazyCCow <1170127589@qq.com>
2016-11-19 00:52:22 +0800

    Add files via upload

---
+++ src/main/scala/WordCount.scala
@@ -0,0 +1,22 @@
+
+import java.text.{SimpleDateFormat}
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+
+/**
+ * Created by daozhang on 2016/11/18.
+ */
+object WordCount {
+  def main(args: Array[String]) {
+    if (args.length == 0) {
+      System.err.println("Usage:  <file1>")
+      System.exit(1)
+    }
+    val sdf = new SimpleDateFormat("HH:mm:ss")
+    val conf = new SparkConf().setMaster("local[*]").setAppName("SogouCount")
+    val sc = new SparkContext(conf)
+    val line = sc.textFile(args(0))
+    println(line.map(_.split("\\t")(0)).filter(x => sdf.parse(x).compareTo(sdf.parse("00:00:00"))>=0 && sdf.parse(x).compareTo(sdf.parse("12:00:00"))<=0).count)
+    sc.stop()
+  }
+}
---
+++ src/main/scala/WordCountB.scala
@@ -0,0 +1,18 @@
+import org.apache.spark.{SparkContext, SparkConf}
+/**
+ * Created by daozhang on 2016/11/19.
+ */
+object SogouCountB {
+    def main(args: Array[String]) {
+      if (args.length == 0) {
+        System.err.println("Usage:  <file1>")
+        System.exit(1)
+      }
+
+      val conf = new SparkConf().setMaster("local[*]")setAppName("SogouCountB")
+      val sc = new SparkContext(conf)
+      val sgRDD=sc.textFile(args(0))
+      println(sgRDD.map(_.split('\t')).filter(_.length ==5).map(_(3).split(' ')).filter(_(0).toInt ==1).filter(_(1).toInt ==2).count)
+      sc.stop()
+    }
+}
